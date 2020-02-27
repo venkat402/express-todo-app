b5143693353853f2718b38f26779d81cf2ce681d
Venkat Adapala <venkat.adapala@ahex.co.in>
2020-02-27 17:09:46 +0530

    CRUD operations completed

--- server/routes/route_index.js
+++ server/routes/route_index.js
@@ -46,22 +46,55 @@ router.get('/add_task', function (req, res, next) {
 
 /* GET add task page. */
 router.get('/change_status/:id', function (req, res, next) {
-  res.render('add_task', { title: 'Express' });
+  Task.find({ _id: req.params.id }, function (err, result) {
+    if (result[0].task_status == 'incomplete') {
+      let newvalues = { $set: { task_status: "completed" } };
+      var myquery = { _id: req.params.id };
+      Task.updateOne(myquery, newvalues, function (err, res) {
+        if (err) throw err;
+      });
+    } else {
+      var myquery = { _id: req.params.id };
+      let newvalues = { $set: { task_status: "incomplete" } };
+      Task.updateOne(myquery, newvalues, function (err, res) {
+        if (err) throw err;
+      });
+    }
+  });
+  res.redirect('/');
+
+});
+
+
+/* GET add task page. */
+router.get('/edit_task/:id', function (req, res, next) {
+  Task.find({ _id: req.params.id }, function (err, result) {
+    if (err) throw err;
+    res.render('edit_task', { task: result });
+  });
+});
+
+/* GET add task page. */
+router.post('/edit_task/:id', function (req, res, next) {
+  Task.find({ _id: req.params.id }, function (err, result) {
+    let newvalues = { $set: { task_status: req.body.task_status, task_name: req.body.task_name, task_description: req.body.task_description } };
+    var myquery = { _id: req.params.id };
+    Task.updateOne(myquery, newvalues, function (err, res) {
+      if (err) throw err;
+    });
+  });
+  res.redirect('/');
 });
 
 /* GET post add task page. */
 router.get('/delete_task/:id', function (req, res, next) {
-  db.collection('Task', function (err, collection) {
-    collection.findByIdAndRemove({ _id: req.params.id }, function (err, results) {
-      if (err) {
-        res.send("failed");
-        throw err;  
-      }
-      res.send("success");
-    });
-  });
-
-
+  Task.findByIdAndRemove({ _id: req.params.id }, function (err, results) {
+    if (err) {
+      res.send("failed");
+      throw err;
+    }
+    res.redirect("/")
+  })
 });
 
 
